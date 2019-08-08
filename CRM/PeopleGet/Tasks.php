fa0d789705b99fc5106cb038c4d593603203d0a4
Alan Dixon <alan.g.dixon@gmail.com>
2019-08-08 18:01:45 +0000

    Increase limit from 100 to 2000 and get addresses and phones as well

--- CRM/PeopleGet/Tasks.php
+++ CRM/PeopleGet/Tasks.php
@@ -12,7 +12,10 @@ class CRM_PeopleGet_Tasks {
     $people_service = new Google_Service_PeopleService($myclient->client);
     try {
       $connections = $people_service->people_connections->listPeopleConnections(
-        'people/me', array('personFields' => 'names,emailAddresses'));
+        'people/me', array(
+          'personFields' => 'names,emailAddresses,addresses,phoneNumbers',
+          'pageSize' => '2000'
+        ));
       // CRM_Core_Error::debug_var('connections',$connections);
     }
     catch (Google_Service_Exception $e) { 
@@ -35,6 +38,18 @@ class CRM_PeopleGet_Tasks {
       }
       if (!empty($contact)) {
         $contact['source'] = 'Google Contact Import from  '.$loggedin['display_name'];
+        foreach($person->phoneNumbers as $phone) {
+          $type = empty($phone['type']) ? 'primary' : $phone['type'];
+          $contact['phone'][$type] = $phone['value'];
+        }
+        foreach($person->addresses as $address) {
+          $type = empty($address['type']) ? 'primary' : $address['type'];
+          $contact['address'][$type]['street_address'] = $address['streetAddress'];
+          $contact['address'][$type]['city'] = $address['city'];
+          $contact['address'][$type]['state_province'] = $address['region'];
+          $contact['address'][$type]['country'] = $address['country'];
+          $contact['address'][$type]['postal_code'] = $address['postalCode'];
+        }
         // CRM_Core_Error::debug_var('contact to import',$contact);
         $task = new CRM_Queue_Task(array('CRM_PeopleGet_Tasks', 'ImportContact'), [$contact], 'Processed '.$contact['email']['primary']);
         // add this task to the queue
@@ -49,8 +64,21 @@ class CRM_PeopleGet_Tasks {
       // CRM_Core_Error::debug_var('contact to import', $contact);
       $result = civicrm_api3('Contact', 'get', ['sequential' => 1, 'email' => $contact['email']['primary']]);
       if ($result['count'] == 0) {
-	$contact['contact_type'] = 'Individual';
+        $contact['contact_type'] = 'Individual';
         $contact['api.email']['email'] = $contact['email']['primary'];
+        if (count($contact['phone'])) {
+          $i = 0;
+          foreach($contact['phone']) as $type => $phoneNumber) {
+            $contact['api.phone.create.'.$i++] = array('location_type_id' => $type, 'phone' => $phoneNumber);
+          }
+        }
+        if (count($contact['address'])) {
+          $i = 0;
+          foreach($contact['address']) as $type => $address) {
+            $address['location_type_id'] = $type;
+            $contact['api.address.create.'.$i++] = $address;
+          }
+        }
         $result = civicrm_api3('Contact', 'create', $contact);
       }
       else {
